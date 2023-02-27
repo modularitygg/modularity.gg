e9613f4fb8407c8ffd5ffe041a20070b5e3f55d1
Rick South <rick@natamus.com>
2023-02-27 15:24:42 +0100

    Remove trailing slashes url identifier

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -2,7 +2,7 @@ const discord = [ "pastebot", "publishbot" ];
 
 document.addEventListener('DOMContentLoaded', function() {
 	let path = window.location.pathname;
-	let identifier = path.replace("/u/", "").split("?")[0].trim();
+	let identifier = path.replaceAll("/u/", "").replaceAll("/", "").split("?")[0].trim();
 	
 	if (discord.includes(identifier)) {
 		window.location.replace("https://discord.modularity.gg/" + identifier);
